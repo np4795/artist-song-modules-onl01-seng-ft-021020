12ae7490c7b0cf325833171efbfef391e11a41ee
Nicholas Patel <np4795@yahoo.com>
2020-02-27 22:01:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class Song
-extend Findable::ClassMethods
-    extend Memorable::ClassMethods
-    include Memorable::InstanceMethods
-    include Paramable::InstanceMethods
+  extend Memorable
+  include Paramable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    #@@songs << self
+    self.class.all << self
   end
 
-   def self.find_by_name(name)
+  def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
 
@@ -22,19 +23,9 @@ extend Findable::ClassMethods
     @@songs
   end
 
-   def self.reset_all
-     self.all.clear
-  end
-
-   def self.count
-     self.all.count
-   end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-     name.downcase.gsub(' ', '-')
-   end
+  
 end
