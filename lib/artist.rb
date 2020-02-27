f3feb9ac2d56971b2bf39be9fac96962664e0302
Nicholas Patel <np4795@yahoo.com>
2020-02-27 21:02:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,35 @@
 require 'pry'
 
 class Artist
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Paramable::InstanceMethods
-  include Memorable::InstanceMethods
+    extend Findable::ClassMethods
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
+    include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
-  @@all = []
+  @@artists = []
 
   def initialize
-    super
+    #@@artists << self
     @songs = []
   end
 
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
+
   def self.all
-    @@all
+    @@artists
   end
+
+  # def self.reset_all
+  #   self.all.clear
+  # end
+
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -27,4 +39,8 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
